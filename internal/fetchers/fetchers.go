a8b39fa6e5be9c4dc06fd1d659923df12fed5589
qasimwarraich <qasim.warraich@gmail.com>
2023-04-13 22:12:38 +0200

    refactor: Extract API requesting into separate function

--- internal/fetchers/fetchers.go
+++ internal/fetchers/fetchers.go
@@ -12,27 +12,10 @@ import (
 )
 
 func GetParticipants(token string) participants.Participants {
-	url := os.Getenv("BACKEND_URL")
-
-	req, err := http.NewRequest("GET", url+"/api/collections/participants/records?perPage=420", nil)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	req.Header.Add("Authorization", token)
-
-	res, err := http.DefaultClient.Do(req)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	buf, err := io.ReadAll(res.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
+	res := makeRequest(token, "/api/collections/participants/records?perPage=420")
 
 	var participants participants.Participants
-	err = json.Unmarshal(buf, &participants)
+	err := json.Unmarshal(res, &participants)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -41,14 +24,25 @@ func GetParticipants(token string) participants.Participants {
 }
 
 func GetVolunteers(token string) volunteers.Volunteers {
-	url := os.Getenv("BACKEND_URL")
+	res := makeRequest(token, "/api/collections/volunteers/records?perPage=420")
 
-	req, err := http.NewRequest("GET", url+"/api/collections/volunteers/records?perPage=420", nil)
+	var volunteers volunteers.Volunteers
+	err := json.Unmarshal(res, &volunteers)
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	return volunteers
+}
+
+func makeRequest(token, endpoint string) []byte {
+	url := os.Getenv("BACKEND_URL")
+
+	req, err := http.NewRequest("GET", url+endpoint, nil)
 	req.Header.Add("Authorization", token)
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	res, err := http.DefaultClient.Do(req)
 	if err != nil {
@@ -60,11 +54,5 @@ func GetVolunteers(token string) volunteers.Volunteers {
 		log.Fatal(err)
 	}
 
-	var volunteers volunteers.Volunteers
-	err = json.Unmarshal(buf, &volunteers)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	return volunteers
+	return buf
 }
